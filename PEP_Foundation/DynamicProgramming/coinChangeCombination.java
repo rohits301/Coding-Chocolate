a36c6d7ab20a3691290631f8496e6d0767f1f7d3
Rohit Sachdeve <sachdevarohit5@gmail.com>
2021-02-04 23:49:02 +0530

    added coin change Combo

---
+++ PEP_Foundation/DynamicProgramming/coinChangeCombination.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        int amt = scn.nextInt();
+        
+        int ans = ccCombinations(arr, amt);
+        System.out.println(ans);
+    }
+    
+    private static int ccCombinations(int[] arr, int amt){
+        int[] dp = new int[amt + 1];
+        
+        dp[0] = 1; 
+        
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < dp.length; j++){
+                if(j - arr[i] >= 0){
+                    dp[j] += dp[j - arr[i]];
+                }
+            }
+        }
+        return dp[amt];
+    }
+}
