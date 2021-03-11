e359249ed473064aef3d1e92656db4dc5357b303
Rohit Sachdeve <sachdevarohit5@gmail.com>
2021-03-11 23:43:10 +0530

    Added Flood Fill Algorithm, different from PEP_FF

---
+++ OnlinePortals/GFG/MustDoGFG/Recursion/floodFillAlgorithm.java
@@ -0,0 +1,80 @@
+// { Driver Code Starts
+    import java.util.*;
+    import java.lang.*;
+    import java.io.*;
+    class GFG
+    {
+        public static void main(String[] args) throws IOException
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int T = Integer.parseInt(br.readLine().trim());
+            while(T-->0)
+            {
+                String[] S1 = br.readLine().trim().split(" ");
+                int n = Integer.parseInt(S1[0]);
+                int m = Integer.parseInt(S1[1]);
+                int[][] image =  new int[n][m];
+                for(int i = 0; i < n; i++){
+                    String[] S2 = br.readLine().trim().split(" ");
+                    for(int j = 0; j < m; j++)
+                        image[i][j] = Integer.parseInt(S2[j]);
+                }
+                String[] S3 = br.readLine().trim().split(" ");
+                int sr = Integer.parseInt(S3[0]);
+                int sc = Integer.parseInt(S3[1]);
+                int newColor = Integer.parseInt(S3[2]);
+                Solution obj = new Solution();
+                int[][] ans = obj.floodFill(image, sr, sc, newColor);
+                for(int i = 0; i < ans.length; i++){
+                    for(int j = 0; j < ans[i].length; j++)
+                        System.out.print(ans[i][j] + " ");
+                    System.out.println();
+                }
+            }
+        }
+    }
+    // } Driver Code Ends
+    
+    
+    /*
+    Test Case:
+    8 8 // m, n
+    1 1 1 1 1 1 1 1
+    1 1 1 1 1 1 0 0
+    1 0 0 1 1 0 1 1
+    1 3 3 3 3 0 1 0
+    1 1 1 3 3 0 1 0
+    1 1 1 3 3 3 3 0
+    1 1 1 1 1 3 1 1
+    1 1 1 1 1 3 3 1
+    4 4 2 // sr, sc, newColor
+    */
+    class Solution
+    {
+        public int[][] floodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            // Code here 
+            
+            int myColor = image[sr][sc];
+            // System.out.println(myColor);
+            floodfillHelper(image, sr, sc, newColor, myColor);
+            // for(int[] val : image){
+            //     for(int v : val){
+            //         System.out.print(v + " ");
+            //     }
+            //     System.out.println();
+            // }
+            return image;
+        }
+        public void floodfillHelper(int[][] image, int sr, int sc, int newColor, int myColor){
+            if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != myColor || image[sr][sc] == newColor){
+                return;
+            } 
+            
+            image[sr][sc] = newColor;
+            floodfillHelper(image, sr, sc - 1, newColor, myColor);
+            floodfillHelper(image, sr - 1, sc, newColor, myColor);
+            floodfillHelper(image, sr, sc + 1, newColor, myColor);
+            floodfillHelper(image, sr + 1, sc, newColor, myColor);
+        }
+    }
