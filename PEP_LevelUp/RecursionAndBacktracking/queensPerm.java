e21a2293a8e8026e224c9045929c9cbe7695d1a1
Rohit Sachdeve <sachdevarohit5@gmail.com>
2021-04-06 18:50:04 +0530

    Added Queens Perm 2D - Queen Choice

--- PEP_LevelUp/RecursionAndBacktracking/queensPerm.java
+++ PEP_LevelUp/RecursionAndBacktracking/queensPerm.java
@@ -3,16 +3,14 @@ import java.util.*;
 
 public class Main {
 
-    public static void queensPermutations(int qpsf, int tq, int[][] chess) {
-        // write your code here
-
-        if (qpsf == tq) {
-            for (int i = 0; i < chess.length; i++) {
-                for (int j = 0; j < chess[0].length; j++) {
-                    if (chess[i][j] == 0) {
-                        System.out.print("-\t");
-                    } else {
+    public static void queensPermutations(int qpsf, int tq, int[][] chess){
+        if(qpsf == tq){
+            for(int i = 0; i < chess.length; i++){
+                for(int j = 0; j < chess[0].length; j++){
+                    if(chess[i][j] != 0){
                         System.out.print("q" + chess[i][j] + "\t");
+                    } else {
+                        System.out.print("-\t");
                     }
                 }
                 System.out.println();
@@ -20,23 +18,22 @@ public class Main {
             System.out.println();
             return;
         }
-
-        for (int i = 0; i < chess.length; i++) {
-            for (int j = 0; j < chess[0].length; j++) {
-                if (chess[i][j] == 0) {
-                    chess[i][j] = (qpsf + 1); // since intially qpsf = 0
+        
+        for(int i = 0; i < chess.length; i++){
+            for(int j = 0; j < chess[0].length; j++){
+                if(chess[i][j] == 0){
+                    chess[i][j] = (qpsf + 1); // (qpsf)th queen placed
                     queensPermutations(qpsf + 1, tq, chess);
                     chess[i][j] = 0;
                 }
             }
         }
     }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[][] chess = new int[n][n];
-
+        
         queensPermutations(0, n, chess);
     }
 }
