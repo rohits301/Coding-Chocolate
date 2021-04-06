e58192574b417bba483be2c36fdbfe382a0e6eaa
Rohit Sachdeve <sachdevarohit5@gmail.com>
2021-04-06 18:35:58 +0530

    Added comments to Queens Combo 2D - Box Choice

--- PEP_LevelUp/RecursionAndBacktracking/queensCombo.java
+++ PEP_LevelUp/RecursionAndBacktracking/queensCombo.java
@@ -3,43 +3,42 @@ import java.util.*;
 
 public class Main {
 
-    public static void queensCombinations(int qpsf, int tq, int row, int col, String asf) {
-        // write your code here
-
-        if (row == tq) {
-            if (qpsf == tq) {
+    public static void queensCombinations(int qpsf, int tq, int row, int col, String asf){
+        // tq => (chess[0].length == chess.length)
+        
+        if(row == tq){
+            if(qpsf == tq){
                 System.out.println(asf);
             }
             return;
         }
-
+        
         int nr = 0;
         int nc = 0;
-
-        String yes = "";
-        String no = "";
-
-        if (col == tq - 1) {
+        
+        String yes = asf;
+        String no = asf;
+        
+        if(col == (tq - 1)){
             nr = row + 1;
             nc = 0;
-            yes = asf + "q\n";
-            no = asf + "-\n";
+            yes += "q\n";
+            no += "-\n";
         } else {
             nr = row;
             nc = col + 1;
-            yes = asf + "q";
-            no = asf + "-";
+            yes += "q";
+            no += "-";
         }
-
-        queensCombinations(qpsf + 1, tq, nr, nc, yes);
-        queensCombinations(qpsf, tq, nr, nc, no);
-
+        
+        queensCombinations(qpsf + 1, tq, nr, nc, yes); // yes 
+        queensCombinations(qpsf, tq, nr, nc, no); // no 
     }
-
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
+        
         queensCombinations(0, n, 0, 0, "");
     }
 }
