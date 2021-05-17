e202d17166a65879b2e7106cd991ae5586df52cb
Rohit Sachdeve <sachdevarohit5@gmail.com>
2021-05-17 15:16:21 +0530

    Added LC.268 Missing Number in Array

--- OnlinePortals/LeetCode/MustDoGFG/Math/LC_268.java
+++ OnlinePortals/LeetCode/MustDoGFG/Math/LC_268.java
@@ -1,19 +1,18 @@
-package MustDoGFG.Math;
 class Solution {
     public int missingNumber(int[] nums) {
-        
+
         int n = nums.length;
 
         int nxor = 0;
-        for(int i = 0; i <= n; i++){
+        for (int i = 0; i <= n; i++) {
             nxor ^= i;
         }
-        
+
         int arrXor = 0;
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             arrXor ^= nums[i];
         }
-        
+
         return (arrXor ^ nxor); // the missing no.
     }
     // Time: O(n)
