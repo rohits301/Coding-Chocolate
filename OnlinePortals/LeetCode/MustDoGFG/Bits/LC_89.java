a98e527a421e6ec03be008c9749c8616698ffbd8
Rohit Sachdeve <sachdevarohit5@gmail.com>
2021-08-02 15:00:58 +0530

    LC.89 Gray Code (ans as integer list)

---
+++ OnlinePortals/LeetCode/MustDoGFG/Bits/LC_89.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<>();
+        ans.add(0);
+
+        if (n == 0)
+            return ans;
+
+        ans.add(1);
+
+        int curr = 1;
+        for (int i = 2; i <= n; i++) {
+            curr *= 2;
+            // traverse the list from back and append curr to all values
+            for (int j = ans.size() - 1; j >= 0; j--) {
+                ans.add(curr + ans.get(j));
+            }
+        }
+        return ans;
+    }
+}
