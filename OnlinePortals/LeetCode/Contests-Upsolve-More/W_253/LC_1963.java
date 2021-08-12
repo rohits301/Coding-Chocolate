a80dc2e1c5e5fd64813ee2640f2815611bbedf57
Rohit Sachdeve <sachdevarohit5@gmail.com>
2021-08-12 23:08:09 +0530

    LC.1963 Minimum Number of Swaps to Make the String Balanced

---
+++ OnlinePortals/LeetCode/Contests-Upsolve-More/W_253/LC_1963.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSwaps(String s) {
+        
+        int stackSize = 0;
+        
+        for(char ch : s.toCharArray()){
+            
+            if(ch == '['){
+                stackSize++;
+            } else {
+                if(stackSize > 0) stackSize--; // balance the brackets
+                
+                else stackSize++; // counting unbalanced paranthesis
+            }
+        }
+        
+        return (stackSize + 1)/2; // total swaps required
+    }
+}
